decf940a3e57d4d066c5bacf2a9a0a1d4b26052d
S331460873 <S331460873@LDHS-BCT-100761.ad.ocdsb.ca>
2019-11-08 10:01:37 -0500

    Delete more useless things

--- 2PlayerSnake/src/pkg2playersnake/Snake.java
+++ 2PlayerSnake/src/pkg2playersnake/Snake.java
@@ -5,15 +5,10 @@
  */
 package pkg2playersnake;
 
-import java.util.ArrayList;
 /**
  *
  * @author S331460873
  */
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class Snake{
@@ -48,7 +43,7 @@ public class Snake{
             left = true;
             right = false;
             up = false;
-            down = false;;
+            down = false;
         }
         if (key == rightKey){
             right = true;
